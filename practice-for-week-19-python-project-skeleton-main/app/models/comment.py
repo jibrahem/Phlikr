7799bf1106ff9aad2e7b98d57192e82fee266f58
isaiah <isaiahcodesforgood@gmail.com>
2023-06-30 15:16:20 -0700

    added seeders

--- practice-for-week-19-python-project-skeleton-main/app/models/comment.py
+++ practice-for-week-19-python-project-skeleton-main/app/models/comment.py
@@ -1,27 +1,23 @@
-from flask_sqlalchemy import SQLAlchemy
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy.orm import validates
-from .user import User
-# from .image import Image
-
-
-db = SQLAlchemy()
 
 class Comment(db.Model):
     __tablename__ = "comments"
 
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(500))
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    image_id = db.Column(db.Integer, db.ForeignKey("images.id"))
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # image_id = db.Column(db.Integer, db.ForeignKey("images.id"))
+
     user = db.relationship("User", back_populates="comments")
     # image = db.relationship(Image, back_populates="comments")
     # print(user)
 
     @validates("description")
     def validate_description(self, key, value):
-        if value < 1:
+        if len(value) < 1:
             raise ValueError('Description must have at least 1 character.')
         return value
 
@@ -30,8 +26,8 @@ class Comment(db.Model):
             "id": self.id,
             "description": self.description,
             "user_id": self.user_id,
-            "image_id": self.image_id,
+            # "image_id": self.image_id,
             "created_at": self.created_at,
             "updated_at": self.updated_at,
-            # "user": self.user
+            "user": self.user
         }
