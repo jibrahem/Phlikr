fcaca0d2d2c5855155c9315a2de5ad14c3a5fecd
juniper <benklee127@gmail.com>
2023-06-30 18:59:09 -0400

    basic DB setup

--- practice-for-week-19-python-project-skeleton-main/app/models/comment.py
+++ practice-for-week-19-python-project-skeleton-main/app/models/comment.py
@@ -9,24 +9,24 @@ class Comment(db.Model):
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    # image_id = db.Column(db.Integer, db.ForeignKey("images.id"))
+    image_id = db.Column(db.Integer, db.ForeignKey("images.id"))
 
     user = db.relationship("User", back_populates="comments")
-    # image = db.relationship(Image, back_populates="comments")
+    image = db.relationship("Image", back_populates="comments")
     # print(user)
 
-    @validates("description")
-    def validate_description(self, key, value):
-        if len(value) < 1:
-            raise ValueError('Description must have at least 1 character.')
-        return value
+    # @validates("description")
+    # def validate_description(self, key, value):
+    #     if len(value) < 1:
+    #         raise ('Description must have at least 1 character.')
+    #     return valueValueError
 
     def to_dict(self):
         return {
             "id": self.id,
             "description": self.description,
             "user_id": self.user_id,
-            # "image_id": self.image_id,
+            "image_id": self.image_id,
             "created_at": self.created_at,
             "updated_at": self.updated_at,
             "user": self.user
