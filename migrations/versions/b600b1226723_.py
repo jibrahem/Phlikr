d244057d54f01be435ba7b85adcb2f4c696b856a
JehanIbrahem <jibrahem@ucsb.edu>
2023-07-08 21:58:49 -0700

    fixed migration

--- migrations/versions/b600b1226723_.py
+++ migrations/versions/b600b1226723_.py
@@ -9,6 +9,11 @@ from alembic import op
 import sqlalchemy as sa
 
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
+
 # revision identifiers, used by Alembic.
 revision = 'b600b1226723'
 down_revision = None
