e4ddd624d1c63b6a3100795ae6ee375163554c52
Benjamin K  Lee <benklee127@gmail.com>
2023-08-07 13:54:09 -0400

    Update SingleImage.js
    removed footer

--- react-app/src/components/Image/SingleImage.js
+++ react-app/src/components/Image/SingleImage.js
@@ -256,7 +256,7 @@ export default function SingleImage() {
                     </div>
                 </div>
             <div>
-              <Footer />
+              // <Footer />
             </div>
           </div>
     </>
