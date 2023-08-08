6e8a8034a3cced8cf2a81363718e229e5fdb814a
JehanIbrahem <jibrahem@ucsb.edu>
2023-08-08 14:52:15 -0700

    took out a part someone already fixed

--- react-app/src/components/UpdatePhoto/index.js
+++ react-app/src/components/UpdatePhoto/index.js
@@ -12,9 +12,9 @@ export default function UpdatePhoto() {
   console.log("editImg in update image component: ", editImg);
   const [errors, setErrors] = useState({});
   // const [title, setTitle] = useState(sessionUser.title);
-  const [editTitle, setTitle] = useState(editImg.title);
+  const [editTitle, setTitle] = useState("");
   // const [description, setDescription] = useState(sessionUser.description);
-  const [editDescription, setDescription] = useState(editImg.description);
+  const [editDescription, setDescription] = useState("");
   // const [img, setImg] = useState(sessionUser.img);
   const [editImgUrl, setImg] = useState(editImg.img);
 
